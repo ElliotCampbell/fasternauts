b6557cff9050f61bfb60370e7957a7ead7bac722
ElliotCampbell <elliotcampbell123@gmail.com>
2015-05-20 17:16:05 -0700

    got the recctangle to move with wasd

--- graphicstest.py
+++ graphicstest.py
@@ -1,10 +1,33 @@
 from graphics import *
+from Tkinter import *
 
 def main():
-    win = GraphWin("My Circle", 900, 700)
-    c = Circle(Point(50,50), 10)
-    c.draw(win)
-    win.getMouse() # pause for click in window
-    win.close()
+
+    root = Tk()
+    frame = Frame(root, width=900, height=700)
+
+    def movement(event):
+    	dir = event.char
+
+    	if dir is "a":
+    		canvas.move(rect, -10, 0)
+    	elif dir is "d":
+    		canvas.move(rect, 10, 0)
+    	elif dir is "s":
+    		canvas.move(rect, 0, 10)
+    	elif dir is "w":
+    		canvas.move(rect, 0, -10)
+
+    	
+    canvas = Canvas(frame, width=900, height=700)
+    rect = canvas.create_rectangle(50, 25, 150, 75, fill="blue")
+    canvas.bind("<Button-1>", lambda event: canvas.focus_set())
+    canvas.bind("<Key>", movement)
+    frame.pack()
+    canvas.pack()
+    
+
+    root.mainloop()
 
 main()
+
